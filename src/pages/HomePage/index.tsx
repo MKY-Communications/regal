57171a613f594d5198bb9e028d09029bd3653ac1
Waheed Khaled <waheedkhaled95@gmail.com>
2025-02-18 21:58:04 +0200

    3 sections left and smooth scroll functionality

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,13 @@
 import "./HomePage.css";
+import "../ShadyPage/ShadyPage.css";
 import Header from "../../components/Header";
 import Screen, { Section } from "../../components/ui/Containers/Container";
 import Footer from "../../components/ui/Footer/Footer";
-import { GlobalExposure, HomePageHeaderOne, HomePageHeaderTwo } from "../../assets/images/home-page";
+import { CultivatingCreativity, DevelopingEssential, GlobalExposure, HomePageHeaderOne, HomePageHeaderTwo, NurturingCuriosity, RegalAdvantage, RegalLegacy } from "../../assets/images/home-page";
 import { RegalStarSVG } from "../../assets/icons/RegalStarSVG";
-import { ArcIcon, BottomSnail, TopSnail } from "../../assets/icons";
+import { ArcIcon, BottomSnail, BritishCambridge, GloballyRecognizedCertificate, SekIesApproach, TopSnail } from "../../assets/icons";
 import ExtendableCard from "../../components/ui/Cards/ExpandableCard";
+import { EducationCard, ImageOverlay, ImageTextCard } from "../ShadyPage/ShadyPage";
 
 const HomePage: React.FC = (): JSX.Element => {
     return (
@@ -60,6 +62,126 @@ const HomePage: React.FC = (): JSX.Element => {
                                 About us
                             </p>
                         </div>
+                    </div>
+                </Section>
+                <Section screenHeight>
+                    <div className="regal-second-section">
+                        <ImageTextCard
+                            title={
+                            <h1 className="image-text-card-title">
+                                Regal's Legacy and <span className="orange-title">Global</span>{" "}
+                                Reach
+                            </h1>
+                            }
+                            subtitle="For over 132 years, Regal Cairo International School joins a legacy of excellence as the 26th school in the 17th country under SEK-IES management. Offering an internationally recognized Cambridge curriculum, Regal CIS is part of a global network committed to shaping bright minds."
+                            image={<img src={RegalLegacy} className="text-card-image" />}
+                        />
+                        <ImageTextCard
+                            title={
+                            <h1 className="image-text-card-title">
+                                The <span className="orange-title">Regal </span>Advantage
+                            </h1>
+                            }
+                            subtitle="Regal Cairo International School offers an internationally recognized Cambridge curriculum, providing students with a world-class education that prepares them for higher education and future careers."
+                            image={<img src={RegalAdvantage} className="text-card-image" />}
+                            reverse
+                        />
+                    </div>
+                </Section>
+                <Section screenHeight>
+                    <div className="regal-third-section">
+                        <div className="education-title-container">
+                            <p className="education-card-subtitle">
+                            Academic Excellence and Global Recognition
+                            </p>
+                            <h2 className="foundations-title education-title">
+                            What Makes Our Education Exceptional?
+                            </h2>
+                        </div>
+                        <div className="education-cards-container">
+                            <EducationCard
+                            src={BritishCambridge}
+                            title="British Cambridge Curriculum"
+                            subtitle=" A globally recognized framework that promotes analytical thinking, problem-solving, and research-based learning."
+                            subtitleaddcss="education-card-sub-title-max-width-268px "
+                            />
+                            <EducationCard
+                            src={SekIesApproach}
+                            title="SEK-IES Approach"
+                            subtitle=" A student-centered philosophy that encourages creativity, collaboration, and responsible action through real-world challenges"
+                            subtitleaddcss="education-card-sub-title-max-width-307px "
+                            titleaddcss="education-card-title-max-width-195px"
+                            />
+                            <EducationCard
+                            src={GloballyRecognizedCertificate}
+                            title="Globally Recognized Certification"
+                            subtitle="Regal CIS graduates receive internationally accepted qualifications that open doors to top universities worldwide, including institutions in the UK, the US, and Europe."
+                            />
+                        </div>
+                    </div>
+                </Section>
+                <Section screenHeight>
+                    <div className="regal-fourth-section">
+                        <div className="foundations-title-container">
+                            <h1 className="foundations-title">
+                            Building Foundations for Lifelong Success
+                            </h1>
+                            <p className="foundations-subtitle">
+                            At{" "}
+                            <span className="orange-p ">Regal Cairo International School</span>{" "}
+                            , we believe that the early years are the most critical in shaping a
+                            child's future. Our Early years programme is designed to provide a
+                            solid foundation for lifelong learning and success. We Lay the
+                            Groundwork for Excellence through
+                            </p>
+                        </div>
+                        <ImageTextCard
+                            title={
+                            <h2 className="card-with-overlay-title">Nurturing Curiosity: </h2>
+                            }
+                            subtitle="Our play-based curriculum fosters a love of learning, encouraging children to explore,discover and grow."
+                            image={
+                            <ImageOverlay
+                                src={NurturingCuriosity}
+                                alt="nurturing"
+                                overlay="Nurturing Curiosity"
+                            />
+                            }
+                            addcss="column-reverse-mobile"
+                        />
+                        <ImageTextCard
+                            title={
+                            <h2 className="card-with-overlay-title">
+                                Developing Essential Skill:{" "}
+                            </h2>
+                            }
+                            subtitle="Developing Essential Skills: We Focus on Building critical Skills like literacy, numeracy, and social-emotional learning, essential for future academic success."
+                            image={
+                            <ImageOverlay
+                                src={DevelopingEssential}
+                                alt="skill"
+                                overlay="Developing Essential Skill"
+                            />
+                            }
+                            reverse
+                            addcss="column-reverse-mobile"
+                        />
+                        <ImageTextCard
+                            title={
+                            <h2 className="card-with-overlay-title">
+                                Cultivating Creativity and Confidence:
+                            </h2>
+                            }
+                            subtitle="Cultivating Creativity and Confidence: Our experienced teachers foster a supportive environment where children can express themselves, take risks, and build confidence."
+                            image={
+                            <ImageOverlay
+                                src={CultivatingCreativity}
+                                alt="cultivating"
+                                overlay="Cultivating Creativity and Confidence"
+                            />
+                            }
+                            addcss="column-reverse-mobile"
+                        />
                     </div>
                 </Section>
                 <Section
