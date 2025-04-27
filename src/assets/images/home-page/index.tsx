5cca9b470e8fb11ba3c01ab19984c4b6bb5cb316
Waheed Khaled <waheedkhaled95@gmail.com>
2025-04-27 12:36:27 +0300

    Regal Legacy Image Changed

--- src/assets/images/home-page/index.tsx
+++ src/assets/images/home-page/index.tsx
@@ -5,4 +5,4 @@ export {default as HomePageHeaderOne} from "./home-page-header-one.jpeg";
 export {default as HomePageHeaderTwo} from "./home-page-header-two.jpeg";
 export {default as NurturingCuriosity} from "./nurturing-curiosity.jpeg";
 export {default as RegalAdvantage} from "./regal-advantage.jpeg";
-export {default as RegalLegacy} from "./regal-legacy.jpeg";
+export {default as RegalLegacy} from "./regal-legacy.webp";
